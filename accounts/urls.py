be5347017b175ec069f25fe7d02132cd69dc5b1e
Tommi <tommi.keinonen@gmail.com>
2021-03-24 12:16:17 +0200

    Fixed wrong redirections

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-
 urlpatterns = [
-    path('', views.LoggedInView.as_view(), name='dashboard'),
+    path('', views.LoggedInView.as_view(), name='loggedin'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
